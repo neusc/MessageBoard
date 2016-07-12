0e94ab57dc2673452690f3140fa199d3a43bba86
neusc <levisc829@gmail.com>
2016-07-12 21:02:40 +0800

    modified:   css/main.css 	modified:   script/board.js

--- script/board.js
+++ script/board.js
@@ -6,33 +6,49 @@
  +-------------------------- */
 var get = {
     byId: function (id) {
-        return typeof id === "string" ? document.getElementById(id) : id
+        return typeof id === "string" ? document.getElementById(id) : id;
     },
+    //元素class属性可能包含多个值
     byClass: function (sClass, oParent) {
         var aClass = [];
         var reClass = new RegExp("(^| )" + sClass + "( |$)");
         var aElem = this.byTagName("*", oParent);
-        for (var i = 0; i < aElem.length; i++) reClass.test(aElem[i].className) && aClass.push(aElem[i]);
-        return aClass
+        for (var i = 0; i < aElem.length; i++) {
+            reClass.test(aElem[i].className) && aClass.push(aElem[i]);
+        }
+        return aClass;
     },
-    byTagName: function (elem, obj) {
-        return (obj || document).getElementsByTagName(elem)
+    byTagName: function (element, obj) {
+        return (obj || document).getElementsByTagName(element);
     }
 };
 /*-------------------------- +
  事件绑定, 删除
  +-------------------------- */
 var EventUtil = {
-    addHandler: function (oElement, sEvent, fnHandler) {
-        oElement.addEventListener ? oElement.addEventListener(sEvent, fnHandler, false) : (oElement["_" + sEvent + fnHandler] = fnHandler, oElement[sEvent + fnHandler] = function () {
-            oElement["_" + sEvent + fnHandler]()
-        }, oElement.attachEvent("on" + sEvent, oElement[sEvent + fnHandler]))
+    addHandler: function (element, type, handler) {
+        if (element.addEventListener) {
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = handler;
+        }
     },
-    removeHandler: function (oElement, sEvent, fnHandler) {
-        oElement.removeEventListener ? oElement.removeEventListener(sEvent, fnHandler, false) : oElement.detachEvent("on" + sEvent, oElement[sEvent + fnHandler])
+    removeHandler: function (element, type, handler) {
+        if (element.removeEventListener) {
+            element.removeEventListener(type, handler, false);
+        } else if (element.detachEvent) {
+            element.detachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = null;
+        }
     },
-    addLoadHandler: function (fnHandler) {
-        this.addHandler(window, "load", fnHandler)
+    addLoadHandler: function (handler) {
+        this.addHandler(window, "load", handler);
+    },
+    getEvent: function (event) {
+        return event ? event : window.event;
     }
 };
 /*-------------------------- +
@@ -43,10 +59,12 @@ function css(obj, attr, value) {
     switch (arguments.length) {
         case 2:
             if (typeof arguments[1] == "object") {
-                for (var i in attr) i == "opacity" ? (obj.style["filter"] = "alpha(opacity=" + attr[i] + ")", obj.style[i] = attr[i] / 100) : obj.style[i] = attr[i];
+                for (var i in attr) {
+                    i == "opacity" ? (obj.style["filter"] = "alpha(opacity=" + attr[i] + ")", obj.style[i] = attr[i] / 100) : obj.style[i] = attr[i];
+                }
             }
             else {
-                return obj.currentStyle ? obj.currentStyle[attr] : getComputedStyle(obj, null)[attr]
+                return obj.currentStyle ? obj.currentStyle[attr] : getComputedStyle(obj, null)[attr];
             }
             break;
         case 3:
@@ -75,7 +93,7 @@ EventUtil.addLoadHandler(function () {
 
     //禁止表单提交
     EventUtil.addHandler(get.byTagName("form", oMsgBox)[0], "submit", function () {
-        return false
+        return false;
     });
 
     //为广播按钮绑定发送事件
@@ -83,26 +101,32 @@ EventUtil.addLoadHandler(function () {
 
     //为Ctrl+Enter快捷键绑定发送事件
     EventUtil.addHandler(document, "keyup", function (event) {
-        var event = event || window.event;
-        event.ctrlKey && event.keyCode == 13 && fnSend()
+        var event = EventUtil.getEvent(event);
+        if (event.ctrlKey && event.keyCode == 13) {
+            fnSend();
+        }
     });
 
     //发送广播函数
     function fnSend() {
-        var reg = /^\s*$/g;
+        var reg = /^\s*$/g; //空白字符
         if (reg.test(oUserName.value)) {
             alert("\u8bf7\u586b\u5199\u60a8\u7684\u59d3\u540d");
             oUserName.focus()
         }
+        // /[\u4e00-\u9fa5]/用于匹配单个汉字，\w等同于	[a-zA-Z_0-9]
         else if (!/^[u4e00-\u9fa5\w]{2,8}$/g.test(oUserName.value)) {
+            //'姓名由2-8位字母、数字、下划线、汉字组成！'
             alert("\u59d3\u540d\u75312-8\u4f4d\u5b57\u6bcd\u3001\u6570\u5b57\u3001\u4e0b\u5212\u7ebf\u3001\u6c49\u5b57\u7ec4\u6210\uff01");
-            oUserName.focus()
+            oUserName.focus();
         }
         else if (reg.test(oConBox.value)) {
+            //随便说点什么吧！
             alert("\u968f\u4fbf\u8bf4\u70b9\u4ec0\u4e48\u5427\uff01");
             oConBox.focus()
         }
         else if (!bSend) {
+            //你输入的内容已超出限制，请检查！
             alert("\u4f60\u8f93\u5165\u7684\u5185\u5bb9\u5df2\u8d85\u51fa\u9650\u5236\uff0c\u8bf7\u68c0\u67e5\uff01");
             oConBox.focus()
         }
@@ -115,18 +139,23 @@ EventUtil.addLoadHandler(function () {
 								<div class=\"msgInfo\">" + oConBox.value.replace(/<[^>]*>|&nbsp;/ig, "") + "</div>\
 								<div class=\"times\"><span>" + format(oDate.getMonth() + 1) + "\u6708" + format(oDate.getDate()) + "\u65e5 " + format(oDate.getHours()) + ":" + format(oDate.getMinutes()) + "</span><a class=\"del\" href=\"javascript:;\">\u5220\u9664</a></div>\
 							 </div>";
-            //插入元素
+
+            //插入元素到留言列表最前面
             aLi.length ? oUl.insertBefore(oLi, aLi[0]) : oUl.appendChild(oLi);
 
             //重置表单
             get.byTagName("form", oMsgBox)[0].reset();
-            for (i = 0; i < aImg.length; i++) aImg[i].className = "";
+            for (i = 0; i < aImg.length; i++) {
+                aImg[i].className = "";
+            }
+            //默认情况下第一个头像为选中状态
             aImg[0].className = "current";
 
             //将元素高度保存
             var iHeight = oLi.clientHeight - parseFloat(css(oLi, "paddingTop")) - parseFloat(css(oLi, "paddingBottom"));
             var alpah = count = 0;
             css(oLi, {"opacity": "0", "height": "0"});
+            //添加的新留言表现高度递增和透明度递增的显示效果
             timer = setInterval(function () {
                 css(oLi, {"display": "block", "opacity": "0", "height": (count += 8) + "px"});
                 if (count > iHeight) {
@@ -134,7 +163,7 @@ EventUtil.addLoadHandler(function () {
                     css(oLi, "height", iHeight + "px");
                     timer = setInterval(function () {
                         css(oLi, "opacity", (alpah += 10));
-                        alpah > 100 && (clearInterval(timer), css(oLi, "opacity", 100))
+                        alpah > 100 && (clearInterval(timer), css(oLi, "opacity", 100));
                     }, 30)
                 }
             }, 30);
@@ -153,9 +182,14 @@ EventUtil.addLoadHandler(function () {
     //输入字符限制
     function confine() {
         var iLen = 0;
-        for (i = 0; i < oConBox.value.length; i++) iLen += /[^\x00-\xff]/g.test(oConBox.value.charAt(i)) ? 1 : 0.5;
+        for (i = 0; i < oConBox.value.length; i++) {
+            //检测输入字符是否是汉字,英文/半角为半个字符，汉字/全角为一个字符
+            iLen += /[^\x00-\xff]/g.test(oConBox.value.charAt(i)) ? 1 : 0.5;
+        }
         oMaxNum.innerHTML = Math.abs(maxNum - Math.floor(iLen));
-        maxNum - Math.floor(iLen) >= 0 ? (css(oMaxNum, "color", ""), oCountTxt.innerHTML = "\u8fd8\u80fd\u8f93\u5165", bSend = true) : (css(oMaxNum, "color", "#f60"), oCountTxt.innerHTML = "\u5df2\u8d85\u51fa", bSend = false)
+        //中文转换为Unicode码
+        maxNum - Math.floor(iLen) >= 0 ? (css(oMaxNum, "color", ""), oCountTxt.innerHTML = "\u8fd8\u80fd\u8f93\u5165", bSend = true)
+            : (css(oMaxNum, "color", "#f60"), oCountTxt.innerHTML = "\u5df2\u8d85\u51fa", bSend = false);
     }
 
     //加载即调用
@@ -163,13 +197,18 @@ EventUtil.addLoadHandler(function () {
 
     //广播按钮鼠标划过样式
     EventUtil.addHandler(oSendBtn, "mouseover", function () {
-        this.className = "hover"
+        this.className = "hover";
     });
 
     //广播按钮鼠标离开样式
     EventUtil.addHandler(oSendBtn, "mouseout", function () {
-        this.className = ""
+        this.className = "";
     });
+
+
+    /*liHover和delLi两个函数使用立即执行函数表达式方式对新发布的留言没有效果
+     * 将新的留言插入列表之后必须重新调用这两个函数因此不能使用匿名函数
+     * */
 
     //li鼠标划过/离开处理函数
     function liHover() {
@@ -179,6 +218,7 @@ EventUtil.addLoadHandler(function () {
                 this.className = "hover";
                 oTmp = get.byClass("times", this)[0];
                 var aA = get.byTagName("a", oTmp);
+                //为新发布的留言增加删除按钮
                 if (!aA.length) {
                     var oA = document.createElement("a");
                     oA.innerHTML = "删除";
@@ -195,10 +235,11 @@ EventUtil.addLoadHandler(function () {
             EventUtil.addHandler(aLi[i], "mouseout", function () {
                 this.className = "";
                 var oA = get.byTagName("a", get.byClass("times", this)[0])[0];
-                oA.style.display = "none"
+                //隐藏删除链接
+                oA.style.display = "none";
             })
         }
-    }
+    };
 
     liHover();
 
@@ -208,35 +249,42 @@ EventUtil.addLoadHandler(function () {
 
         for (i = 0; i < aA.length; i++) {
             aA[i].onclick = function () {
+                //定位到需要删除的留言列表项li
                 var oParent = this.parentNode.parentNode.parentNode;
                 var alpha = 100;
                 var iHeight = oParent.offsetHeight;
+                //将要删除的留言先变为透明然后在布局上删除，显示出层次上的递进感
                 timer = setInterval(function () {
                     css(oParent, "opacity", (alpha -= 10));
                     if (alpha < 0) {
                         clearInterval(timer);
                         timer = setInterval(function () {
                             iHeight -= 10;
-                            iHeight < 0 && (iHeight = 0);
                             css(oParent, "height", iHeight + "px");
-                            iHeight == 0 && (clearInterval(timer), oUl.removeChild(oParent))
+                            if (iHeight <= 0) {
+                                clearInterval(timer);
+                                oUl.removeChild(oParent);
+                            }
+                            // iHeight < 0 && (iHeight = 0);
+                            // css(oParent, "height", iHeight + "px");
+                            // iHeight == 0 && (clearInterval(timer), oUl.removeChild(oParent));
                         }, 30)
                     }
                 }, 30);
-                this.onclick = null
+                this.onclick = null;
             }
         }
-    }
+    };
 
     delLi();
 
     //输入框获取焦点时样式
     for (i = 0; i < aFtxt.length; i++) {
         EventUtil.addHandler(aFtxt[i], "focus", function () {
-            this.className = "active"
+            this.className = "active";
         });
         EventUtil.addHandler(aFtxt[i], "blur", function () {
-            this.className = ""
+            this.className = "";
         })
     }
 
@@ -248,14 +296,16 @@ EventUtil.addLoadHandler(function () {
     //头像
     for (i = 0; i < aImg.length; i++) {
         aImg[i].onmouseover = function () {
-            this.className += " hover"
+            this.className += " hover";
         };
         aImg[i].onmouseout = function () {
-            this.className = this.className.replace(/\s?hover/, "")
+            this.className = this.className.replace(/\s?hover/, "");
         };
         aImg[i].onclick = function () {
-            for (i = 0; i < aImg.length; i++) aImg[i].className = "";
-            this.className = "current"
+            for (i = 0; i < aImg.length; i++) {
+                aImg[i].className = "";
+            }
+            this.className = "current";
         }
     }
 });
